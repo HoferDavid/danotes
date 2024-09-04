142d202d7c154c77a4f4dbf0ca9a88fb3002cde8
David Hofer <chassis.poppets0x@icloud.com>
2024-09-04 11:00:18 +0200

    Firebase Deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NoteListComponent } from './note-list/note-list.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { Firestore } from '@angular/fire/firestore';
 
 
 @Component({
@@ -17,4 +18,5 @@ import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.compon
 export class AppComponent {
   title = 'danotes';
   addDialogOpen = false;
+  firestore = inject(Firestore);
 }
